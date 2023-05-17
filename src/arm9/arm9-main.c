572c0910b179a626d423d50d4b05836a603a5bbe
V01D-NULL <timster23@protonmail.com>
2023-05-17 13:05:08 -0500

    Make i2c a shared driver

--- src/arm9/arm9-main.c
+++ src/arm9/arm9-main.c
@@ -1,12 +1,15 @@
 
 // #include <shared/lib/print.h>
 #include <core/communication/arm11.h>
+#include <shared/drivers/i2c.h>
 
 void _start(void)
 {
 	// Todo: Assign bottom framebuffer to Arm9. A macro to set the fb_pointer variable would be enough
 	// print("Hello world from the ARM9");
 	send_message(TEST);
+
+	i2c_write(3, 0x20, 1 << 0); // shutdown
 
 	for (;;)
 		;
